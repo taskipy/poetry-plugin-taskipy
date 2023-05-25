3f8b8aedfc94bf5079c74c52e235041dce2067e9
Roy Sommer <royso@wix.com>
2023-05-25 23:54:07 +0300

    getting ready to release

---
+++ .hooks/make_release_commit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import tomli
+import subprocess
+from pathlib import Path
+
+
+def create_release_commit():
+    cwd = Path.cwd()
+    with open(cwd / "pyproject.toml", "rb") as file:
+        pyproject = tomli.load(file)
+    version = pyproject["tool"]["poetry"]["version"]
+
+    p = subprocess.Popen(
+        f'git add . && git commit -m "Release version {version}" && git tag -a "{version}" -m "Release version {version}" && git push && git push --tags',
+        shell=True,
+        cwd=cwd,
+    )
+
+    p.wait()
+
+
+if __name__ == "__main__":
+    create_release_commit()
